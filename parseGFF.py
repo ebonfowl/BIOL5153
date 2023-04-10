def561ca47d749e9394935b520e5d9badd4942c7
ebonfowl <101683149+ebonfowl@users.noreply.github.com>
2023-04-10 09:52:33 -0500

    Version 1.1
    Now outputs each element length and yeets the last extra line

--- parseGFF.py
+++ parseGFF.py
@@ -4,11 +4,11 @@
 import argparse
 
 # create argument parser object
-parser = argparse.ArgumentParser(description='This script reads in a GFF file line by line')
+parser = argparse.ArgumentParser(description = 'This script reads in a GFF file line by line')
 
 # add positional arguments
-parser.add_argument('gff_file', help = 'GFF filename', type = str)
-parser.add_argument('fasta_file', help = 'FASTA filename', type = str)
+parser.add_argument('gff_file', help = 'GFF filepath', type = str)
+parser.add_argument('fasta_file', help = 'FASTA filepath', type = str)
 
 # parse arguments
 args = parser.parse_args()
@@ -16,3 +16,14 @@ args = parser.parse_args()
 # read file
 GFF_file = open(args.gff_file, 'r')
 GFF_lines = GFF_file.readlines()
+GFF_file.close()
+GFF_lines.remove(GFF_lines[-1])
+
+# GFF_lines2 = []
+for line in GFF_lines:
+    # print(line)
+    stripped_line = line.strip()
+    stripped_line2 = stripped_line.split('\t')
+    length = int(stripped_line2[4]) - int(stripped_line2[3])
+    print(length)
+
