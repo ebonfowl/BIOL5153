115bd52b93305df622cd973bbc6134285911c973
ebonfowl <101683149+ebonfowl@users.noreply.github.com>
2023-04-17 09:42:53 -0500

    Version 1.5
    Updated to print FASTA sequence for each feature

--- parseGFF.py
+++ parseGFF.py
@@ -3,6 +3,7 @@
 # import modules
 import argparse
 import csv
+from Bio import SeqIO
 
 # create argument parser object
 parser = argparse.ArgumentParser(description = 'This script reads in a GFF file line by line')
@@ -14,10 +15,11 @@ parser.add_argument('fasta_file', help = 'FASTA filepath', type = str)
 # parse arguments
 args = parser.parse_args()
 
+genome = SeqIO.read(args.fasta_file, "fasta")
+# print(genome)
+
 # read file
 GFF_file = open(args.gff_file, 'r')
-# GFF_lines = GFF_file.readlines()
-GFF_file.close()
 
 reader = csv.reader(GFF_file, delimiter = '\t')
 
@@ -40,30 +42,11 @@ for line in reader:
     strand = line[6]
     attributes = line[8]
 
-    print(feature_type + '\t' + length)
-
-# GFF_lines2 = []
-# for line in GFF_lines:
-    # if line == '\n':
-        # GFF_lines.remove(line)
-        # continue
-    # line = line.strip()
-    # line = line.split('\t')
-
-    # define column variables
-    # organism = line[0]
-    # source = line[1]
-    # feature_type = line[2]
-    # start = int(line[3])
-    # end = int(line[4])
-
-    # fix length
-    # line[5] = str(end - start + 1)
-
-    # length = line[5]
-    # strand = line[6]
-    # attributes = line[8]
-
     # print(feature_type + '\t' + length)
 
-    # GFF_lines2.append(line)
+    sequence = genome.seq[start - 1: end]
+    # header = ' '.join(organism, feature_type, attributes)
+    print('>' + organism, feature_type, attributes)
+    print(sequence)
+
+GFF_file.close()
